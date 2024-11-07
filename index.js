53ed0441431bf057a513cff8d6f887d9c38abd3c
IndieCorreia <1941976@hc.ac.je>
2024-11-07 11:12:26 +0000

    removed javascript connection test comment

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// javascript
-
 alert("What the qharopy?");
 
 // Display a form to the user
@@ -11,16 +9,16 @@ alert("What the qharopy?");
 // Capture user's input on form submission
 // Get the user's email address - userEmail (string)
 // Get the user's level - userLevel (string)
-// Get the user's hours of study - studyHours (number)
+// Get the user's hours of study - userHours (number)
 
 // Validate the user's input
 // Display a form to the user
 // Include input fields for the user's email address, level, and hours of study
 // Capture user's input on form submission
 // Validate the user's input
-// Check if the user has selected a level
+// Check if the user has selected a level - check against allowed level
 // Check if the user has provided an email address
-// Check if the user has specified at least one hour of study - cannot be negative
+// Check if the user has specified at least one hour of study - cannot be negative- must be >= 1 and <= the max number allowed for level
 // Check if the number of hours requested is within the allowed range
 // Calculate the total cost
 // Display the total cost to the user
