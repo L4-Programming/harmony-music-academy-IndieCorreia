7ae4e3ee7fba7c0c6e80318bcb3c0eb71f360b39
IndieCorreia <1941976@hc.ac.je>
2024-11-07 12:16:52 +0000

    Added html IDs and added an event listener
    Event listener checks for a submit event then logs 'Form submitted'

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-console.log("What the qharopy?");
-
-// Display a form to the user
-// Include input fields for the user's email address, level, and hours of study
+console.log("Hai!");
 
 // Capture user's input on form submission
-// Display a form to the user
-// Include input fields for the user's email address, level, and hours of study
-// Capture user's input on form submission
+let form = document.querySelector("form");
+
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  console.log("Form submitted");
+});
 // Get the user's email address - userEmail (string)
 // Get the user's level - userLevel (string)
 // Get the user's hours of study - userHours (number)
 
 // Validate the user's input
-// Display a form to the user
-// Include input fields for the user's email address, level, and hours of study
-// Capture user's input on form submission
-// Validate the user's input
 // Check if the user has selected a level - check against allowed level
 // Check if the user has provided an email address
 // Check if the user has specified at least one hour of study - cannot be negative- must be >= 1 and <= the max number allowed for level
 // Check if the number of hours requested is within the allowed range
-// Calculate the total cost
-// Display the total cost to the user
 
 // Calculate the total cost
 // Display the total cost to the user
