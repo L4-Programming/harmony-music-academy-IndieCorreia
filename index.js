b17e81f8b3b5376abc93c371aa56368d231d9a4e
IndieCorreia <1941976@hc.ac.je>
2024-11-14 11:44:10 +0000

    created content validator for each data field

--- index.js
+++ index.js
@@ -6,17 +6,58 @@ let form = document.querySelector("form");
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
+  const maxHoursPerLevel = {
+    basic: 5,
+    advanced: 10,
+  };
+
   // Store the user's email address as userEmail (string/text)
   let userEmail = document.querySelector("#email").value;
-  console.log({ userEmail });
 
   // Get the user's level - userLevel (string)
   let userLevel = document.querySelector("#level").value;
-  console.log({ userLevel });
 
   // Get the user's hours of study - userHours (number)
-  let userHours = document.querySelector("#hoursPerWeek").value;
-  console.log({ userHours });
+  let userHours = parseInt(document.querySelector("#hoursPerWeek").value);
+
+  // Validate the user's input
+  // Check if the user has provided an email address
+  if (userEmail === "") {
+    alert("Please enter your email address.");
+
+    return;
+  }
+
+  // Check if the user has selected a level - check against allowed level
+  if (userLevel === "") {
+    alert("Please select a level of study.");
+
+    return;
+  }
+
+  // Check if the user has specified at least one hour of study
+  if (isNaN(userHours) || userHours < 1) {
+    alert("Please enter at least 1 hour of study.");
+
+    return;
+  }
+
+  //Is userlevel valid?
+  if (!maxHoursPerLevel.hasOwnProperty(userLevel)) {
+    alert("Invalid level of study selected");
+
+    return;
+  }
+
+  //Is userHours within range?
+  const maxAllowedHours = maxHoursPerLevel[userLevel];
+  if (userHours > maxAllowedHours) {
+    alert(`You can only study a maximum of ${maxAllowedHours} hours per week.`);
+
+    return;
+  }
+
+  console.log({ userEmail, userLevel, userHours });
 });
 
 // Capture users input on form submission
